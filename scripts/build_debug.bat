27db4ab37d9d1396238634e7bea284b316f89de0
admin <admin@users.noreply.github.com>
2022-05-11 10:14:20 +0800

    upl vs2015 proj

--- scripts/build_debug.bat
+++ scripts/build_debug.bat
@@ -1,10 +1,11 @@
 @echo off
 
-set VC_DIR=D:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
+set VC_DIR=D:\Program Files (x86)\Microsoft Visual Studio 14.0\VC
 set MSDK_DIR=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A
-set QT_DIR=D:\Qt\Qt5.4.2\5.4\msvc2013_opengl
-set INCLUDE=%VC_DIR%\include;%MSDK_DIR%\Include
-set LIB=%MSDK_DIR%\Lib;%VC_DIR%\lib
+set QT_DIR=D:\Qt\Qt5.7.1\5.7\msvc2015
+set WIN_KIT=C:\Program Files (x86)\Windows Kits\10
+set INCLUDE=%VC_DIR%\include;%MSDK_DIR%\Include;%WIN_KIT%\Include\10.0.10150.0\ucrt
+set LIB=%MSDK_DIR%\Lib;%VC_DIR%\lib;%WIN_KIT%\Lib\10.0.10150.0\ucrt\x86
 set SRC_DIR=%cd%
 set BUILD_DIR=%cd%\QPoisson
 set PATH=%VC_DIR%\bin;%MSDK_DIR%\Bin;%QT_DIR%\bin
@@ -15,7 +16,7 @@ if not exist %BUILD_DIR% md %BUILD_DIR%
 
 cd QPoisson
 
-%QT_DIR%\bin\qmake.exe %SRC_DIR%\QPoisson.pro -spec win32-msvc2013  "CONFIG+=debug" "CONFIG+=console"
+%QT_DIR%\bin\qmake.exe %SRC_DIR%\QPoisson.pro -spec win32-msvc2015 "CONFIG+=debug" "CONFIG+=console"
 if exist %BUILD_DIR%\Debug\QPoisson.exe del %BUILD_DIR%\Debug\QPoisson.exe
 nmake Debug
 if not exist %BUILD_DIR%\Debug\Qt5Cored.dll (
